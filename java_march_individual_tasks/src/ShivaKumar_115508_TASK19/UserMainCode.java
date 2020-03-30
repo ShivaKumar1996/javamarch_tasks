cff6c4708cf5492817694f54ed3e3fc93e6dcb27
91997 <91997@LAPTOP-PFOJDI0G>
2020-03-30 23:02:23 +0530

    Task Completed

--- java_march_individual_tasks/src/ShivaKumar_115508_TASK19/UserMainCode.java
+++ java_march_individual_tasks/src/ShivaKumar_115508_TASK19/UserMainCode.java
@@ -1,10 +1,26 @@
 package ShivaKumar_115508_TASK19;
 
+import java.util.Scanner;
 public class UserMainCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    int a[] = new int[20];
+	    int n = sc.nextInt();
+	    for (int i = 0; i < n; i++) {
+	      a[i] = sc.nextInt();
+	    }
+	    System.out.println(addEvenOdd(a));
 
 	}
-
+	public static int addEvenOdd(int[] a) {
+	    int n1 = 0, n2 = 0;
+	    for (int i = 0; i < a.length; i++)
+	      if (a[i] % 2 == 0)
+	        n1 += (a[i] * a[i]);
+	      else
+	        n2 += (a[i] * a[i] * a[i]);
+	    return n1 + n2;
+	  }
 }
+
